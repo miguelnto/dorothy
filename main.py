a4867db20e1f59e296596734c87e18b677d465bf
miguelnto <miguelup01@outlook.com>
2024-10-28 19:15:13 -0300

    ...

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from config import conf
 
 def main():
+    # Create directories for dotfiles
+    conf.create_dirs_if_not_exists()
     # Install arch packages
     conf.install_arch_packages()
     # Install git packages, including sah, the AUR helper.
@@ -8,8 +10,6 @@ def main():
     conf.install_git_packages()
     # Install aur packages
     conf.install_aur_packages()
-    # Create directories for dotfiles
-    conf.create_dirs_if_not_exists()
     # Clone git packages, i.e, pull up the dotfiles.
     conf.clone_git_packages()
     # Install the dotfiles 
